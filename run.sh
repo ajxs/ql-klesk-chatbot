02c4cd219396a0756b75cc5ed05ead254420d1f0
Anthony <ajxscc@gmail.com>
2018-04-01 04:12:09 +0100

    Script improvements

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# main program args, edit these.
 BASEQ3_DIR="C:/Program Files (x86)/Steam/steamapps/common/Quake Live/76561198161852807/baseq3"
-PLAYER_NAME="Taylor Thrift"
-tail -f -n 0 "${BASEQ3_DIR}/qconsole.log"  | ./ql-parser.pl --dir "${BASEQ3_DIR}" --name "${PLAYER_NAME}"
+PLAYER_NAME="Klesk"
+
+# Ensure existence of target files.
+KLESK_CFG_TARGET="${BASEQ3_DIR}/klesk.cfg"
+[ -f "${KLESK_CFG_TARGET}" ] || cp "./klesk.cfg" "${KLESK_CFG_TARGET}"
+touch "${BASEQ3_DIR}/klesk-insult.cfg"
+touch "${BASEQ3_DIR}/klesk-insult-talker.cfg"
+touch "${BASEQ3_DIR}/klesk-praise.cfg"
+touch "${BASEQ3_DIR}/klesk-taunt.cfg"
+
+tail -f -n 0 "${BASEQ3_DIR}/qconsole.log"  | ./klesk.pl --dir "${BASEQ3_DIR}" --name "${PLAYER_NAME}"
